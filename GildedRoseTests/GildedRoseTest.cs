c52d8c1de50e628b5f505f4650a5733dc71b19bc
damian dommett <damiandommett@hotmail.co.uk>
2025-03-15 11:01:19 +0000

    added quality reduces test

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -16,12 +16,21 @@ namespace GildedRoseTests
         }
 
         [Fact]
-        public void sell_in_time_reduces()
+        public void sell_in_time_reduces_each_day()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 2, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.Equal(1, Items[0].SellIn);
+        }
+
+        [Fact]
+        public void quality_reduces_each_day()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 2, Quality = 3 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(2, Items[0].Quality);
         }
 
     }

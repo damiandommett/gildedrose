7eb0456f998a98d9c8cb5dac606c1030c6f2d5a5
damian dommett <damiandommett@hotmail.co.uk>
2025-03-15 10:56:53 +0000

    added sell in time reduces test

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -14,5 +14,15 @@ namespace GildedRoseTests
             app.UpdateQuality();
             Assert.Equal("foo", Items[0].Name);
         }
+
+        [Fact]
+        public void sell_in_time_reduces()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 2, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(1, Items[0].SellIn);
+        }
+
     }
 }

e839c8b9380657dcf427538a48b22f68877c4f7d
damian dommett <damiandommett@hotmail.co.uk>
2025-03-15 11:39:33 +0000

    Added sulfuras never declines tests

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -25,6 +25,15 @@ namespace GildedRoseTests
         }
 
         [Fact]
+        public void sulfuras_sell_in_time_does_not_reduce_each_day()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 2, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(2, Items[0].SellIn);
+        }
+
+        [Fact]
         public void quality_reduces_each_day()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 2, Quality = 3 } };
@@ -32,6 +41,16 @@ namespace GildedRoseTests
             app.UpdateQuality();
             Assert.Equal(2, Items[0].Quality);
         }
+
+        [Fact]
+        public void sulfuras_quality_does_not_reduce_each_day()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 4, Quality = 3 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(3, Items[0].Quality);
+        }
+
 
         [Fact]
         public void quality_cannot_be_negative()
